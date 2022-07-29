8c6d40e429e012c540494a5fd811a8c251695ea6
JSH <ertr@coinplug.com>
2022-07-29 16:51:33 +0900

    메인 페이지 반응형 디자인 구현하기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,34 @@
 import React, { FunctionComponent } from 'react'
-import Text from 'components/Text'
+import GlobalStyle from 'components/Common/GlobalStyle'
+import ProfileImage from 'components/Main/ProfileImage'
+import styled from '@emotion/styled'
+import Introduction from 'components/Main/Introduction'
+import Footer from 'components/Common/Footer'
+import CategoryList from 'components/Main/CategoryList'
+import PostList from 'components/Main/PostList'
+
+const CATEGORY_LIST = {
+  제니스: 5,
+  크로아: 3,
+  리부트1: 2,
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`
 
 const IndexPage: FunctionComponent = function () {
-  return <Text text="Home" />
+  return (
+    <Container>
+      <GlobalStyle />
+      <Introduction />
+      <CategoryList selectedCategory="Web" categoryList={CATEGORY_LIST} />
+      <PostList />
+      <Footer />
+    </Container>
+  )
 }
 
 export default IndexPage
